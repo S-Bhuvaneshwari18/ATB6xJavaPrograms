38daab978109d3ec95bd5b7dae012100852390d4
saravanan raja <bhuvipooja18@gmail.com>
2024-05-06 18:54:44 +0530

    set and commit

---
+++ src/ex_04052024/Lab145.java
@@ -0,0 +1,11 @@
+package ex_04052024;
+
+public class Lab145 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		  System.out.println("Hi");
+		 // System.out.println();
+	}
+
+}
